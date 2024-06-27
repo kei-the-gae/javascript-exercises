49a5eb1cec2747d8aae364de38e5b227ea51c006
Kei Holland <85800932+kei-the-gae@users.noreply.github.com>
2024-06-26 21:20:38 -0700

    Add exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,8 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+    if (num < 0) return "ERROR";
+    let repeatString = "";
+    for (i = 0; i < num; i++) { repeatString += string };
+    return repeatString;
 };
 
 // Do not edit below this line
